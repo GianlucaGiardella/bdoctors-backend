19c7a1a2d6d3d5b68c036b091548e78249559082
GianluGiarde <gianlugiarde@gmail.com>
2024-01-19 15:22:58 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <title>Template</title>
+</head>
+
+<body style="height: 100vh">
+    <h1 class="h-100 d-flex justify-content-center align-items-center">BDoctors <i class="fa-solid fa-user-doctor"></i>
+    </h1>
+</body>
+
+</html>
